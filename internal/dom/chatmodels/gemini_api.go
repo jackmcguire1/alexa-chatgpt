2f55ec27280dd12c8f51eeb66fb0a2a6461a88c4
jackmc <jackmcguire35@gmail.com>
2023-12-14 20:53:07 +0000

    fix: do not fatal

--- internal/dom/chatmodels/gemini_api.go
+++ internal/dom/chatmodels/gemini_api.go
@@ -2,7 +2,6 @@ package chatmodels
 
 import (
 	"context"
-	"log"
 
 	"github.com/google/generative-ai-go/genai"
 	"github.com/sashabaranov/go-openai"
@@ -23,7 +22,7 @@ func NewGeminiApiClient(token string) *GeminiApiClient {
 func (api *GeminiApiClient) GeminiChat(ctx context.Context, prompt string) (*genai.GenerateContentResponse, error) {
 	client, err := genai.NewClient(ctx, option.WithCredentialsJSON(api.Token))
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer client.Close()
 	model := client.GenerativeModel("gemini-pro")
