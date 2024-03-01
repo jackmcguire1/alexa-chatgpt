f9615e0cf934d0a3293380fa81103d96cacca568
jackmc <jackmcguire35@gmail.com>
2024-03-01 01:53:59 +0000

    fix: google project id from secret

--- internal/dom/chatmodels/gemini_api.go
+++ internal/dom/chatmodels/gemini_api.go
@@ -1,31 +1,32 @@
 package chatmodels
 
 import (
-	"cloud.google.com/go/vertexai/genai"
 	"context"
 	"encoding/base64"
+	"log/slog"
+
+	"cloud.google.com/go/vertexai/genai"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
-	"log/slog"
 )
 
 type GeminiApiClient struct {
 	credentials *google.Credentials
-	projectID   string
 }
 
-func NewGeminiApiClient(token, projectID string) *GeminiApiClient {
+func NewGeminiApiClient(token string) *GeminiApiClient {
 	tkn, _ := base64.StdEncoding.DecodeString(token)
+
 	creds, err := google.CredentialsFromJSON(context.Background(), tkn, "https://www.googleapis.com/auth/generative-language")
 	if err != nil {
 		slog.With("error", err).Error("failed to init google creds")
 	}
 
-	return &GeminiApiClient{credentials: creds, projectID: projectID}
+	return &GeminiApiClient{credentials: creds}
 }
 
 func (api *GeminiApiClient) GeminiChat(ctx context.Context, prompt string) (*genai.GenerateContentResponse, error) {
-	client, err := genai.NewClient(ctx, api.projectID, "us-central1", option.WithCredentials(api.credentials))
+	client, err := genai.NewClient(ctx, api.credentials.ProjectID, "us-central1", option.WithCredentialsJSON(api.credentials.JSON))
 	if err != nil {
 		return nil, err
 	}
