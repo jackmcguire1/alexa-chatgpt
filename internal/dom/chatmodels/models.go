b4e34d30109254b44f716179d065d06a2d049846
jackmc <jackmcguire35@gmail.com>
2024-03-08 12:13:46 +0000

    fix: awq model const

--- internal/dom/chatmodels/models.go
+++ internal/dom/chatmodels/models.go
@@ -6,7 +6,7 @@ const (
 	CHAT_MODEL_GEMINI ChatModel = "gemini"
 	CHAT_MODEL_GPT    ChatModel = "gpt"
 	CHAT_MODEL_META   ChatModel = "meta"
-	CHAT_MODEL_AWQ    ChatModel = "awsq"
+	CHAT_MODEL_AWQ    ChatModel = "awq"
 	CHAT_MODEL_OPEN   ChatModel = "open chat"
 	CHAT_MODEL_SQL    ChatModel = "sql"
 )
