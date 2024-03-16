8edc633bf1fc4f27e20bf484a6c65538474a12bc
Jack McGuire <jackmcguire35@gmail.com>
2024-03-16 18:50:56 +0000

    fix: use llama

--- internal/dom/chatmodels/models.go
+++ internal/dom/chatmodels/models.go
@@ -5,7 +5,7 @@ type ChatModel string
 const (
 	CHAT_MODEL_GEMINI           ChatModel = "gemini"
 	CHAT_MODEL_GPT              ChatModel = "gpt"
-	CHAT_MODEL_META             ChatModel = "meta"
+	CHAT_MODEL_META             ChatModel = "llama"
 	CHAT_MODEL_AWQ              ChatModel = "awq"
 	CHAT_MODEL_OPEN             ChatModel = "open chat"
 	CHAT_MODEL_SQL              ChatModel = "sql"
